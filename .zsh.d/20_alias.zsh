c578bb4b04b639ee92bf32116efe41918c074249
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2014-07-12 13:41:15 +0200

    chromekill alias

--- .zsh.d/20_alias.zsh
+++ .zsh.d/20_alias.zsh
@@ -1,5 +1,8 @@
 alias ls='ls -FG'
 alias l='ls -A'
-alias ll='ls -Al'
+alias ll='ls -Alh'
 
 alias rr='source $HOME/.zshrc'
+
+# Kill all the tabs in Chrome to free up memory
+alias chromekill="ps ux | grep '[C]hrome Helper --type=renderer' | grep -v extension-process | tr -s ' ' | sed "s/^$(whoami)//g" | cut -d ' ' -f2 | xargs kill"
